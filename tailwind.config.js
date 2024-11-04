0e674bd17d81f744d5abd979ff5514f413f82a78
RuchikaKadam <kadamruchikafullstack@gmail.com>
2024-11-05 01:23:41 +0530

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './index.html',
+    './styles.css',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        orange: '#CB6827',
+        bottleGreen: '#0A3836',
+        yellow: '#F8B642',
+        maroon: '#90263C',
+        purple: '#8C336B',
+        blue: '#2B687D',
+        olive: '#517456',
+        green: '#378247',
+        sky: '#8FD4D1',
+        darkGreen: '#052323'
+      },
+    },
+  },
+  plugins: [],
+}
+
